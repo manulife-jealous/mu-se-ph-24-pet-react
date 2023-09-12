eb7f6a3f4afe91158e47fe6818f164565ee45505
Ian Eco <ian_paul_eco@manulife.ca>
2023-09-11 22:13:04 -0400

    Add delete functionality.

--- src/components/forumfeed/ForumFeed.js
+++ src/components/forumfeed/ForumFeed.js
@@ -1,5 +1,6 @@
 import ForumCard from "../shared/card/ForumCard";
 import { gql, useQuery } from '@apollo/client';
+import { useState, useEffect } from 'react';
 
 const GET_POSTS = gql`
   query GetPosts {
@@ -21,18 +22,33 @@ const GET_POSTS = gql`
   }
 `;
 
-const ForumFeed = () => {
+const ForumFeed = ({post}) => {
   //gql
   const { loading, error, data } = useQuery(GET_POSTS);
   const currentuser= {current_user_id: 1}; //get this from login
 
+  const [currentPost, setPost] = useState(post);
+
   if(loading) return (<p>Loading...</p>);
   if(error) return (<p>Error: {error.message}</p>);
+
+  const handleOnDelete = (post) => {
+    let a = post !== undefined ? data.posts.filter(posty => posty.id != post.id) : undefined;    
+    console.log(post !== undefined ? post.id : 0);
+    setPost(a);
+  
+  }
+
+  console.log(currentPost !== undefined ? currentPost : 0);
+
   return (
     <>
     { !loading && data &&
-      data.posts.map(post => {
-        return (<ForumCard key={post.id} post={post} currentuser={currentuser}></ForumCard>)
+      currentPost === undefined ? data.posts.map(post => {
+        return (<ForumCard onDelete={handleOnDelete} key={post.id} post={post} currentuser={currentuser}></ForumCard>);
+      }) :
+      currentPost.map(post => {
+        return (<ForumCard onDelete={handleOnDelete} key={post.id} post={post} currentuser={currentuser}></ForumCard>);
       })
     }
     
--- src/components/shared/card/ForumCard.js
+++ src/components/shared/card/ForumCard.js
@@ -1,12 +1,60 @@
 import React from 'react'
 import { Card, Stack, Button } from '@manulife/mux';
 import * as CDS from '@manulife/mux-cds-icons';
+import {useState} from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-const ForumCard = ({currentuser, post}) =>{
+const GET_POST = gql`
+  query Post($postId: ID) {
+  post(id: $postId) {
+    comments {
+      id
+      message
+      post_id
+    }
+    id
+    message
+    user_id
+    votes {
+      downvotes
+      id
+      post_id
+      upvotes
+    }
+  }
+}
+`;
+
+const ForumCard = ({currentuser, post, onDelete}) =>{
+  const { loading, error, data } = useQuery(GET_POST,
+    {variables: {postId: 1}, });
   const { current_user_id } = currentuser
   const { id, user_id, message } = post;
   const cardStyle = { width: '100%', margin: '1em' };
 
+  const API_URL = 'http://localhost:3001';
+
+  const PostItem = ({post}) => {
+    const [deletePost, setDeletePost] = useState(post);
+  }
+
+  /*
+  useEffect(() => {
+    fetchPost();
+  },[])
+  */
+
+  /*
+  const fetchPost = async () => await fetch(`${API_URL}/posts/${post.id}`)
+    .then(response => response.json())
+    .then(data => setPost(data))
+  */
+    const [deletePost, setDeletePost] = useState();
+    const handleOnDelete = () => {      
+      setDeletePost(post);
+      onDelete(deletePost);      
+  }
+  
   return (
     <Stack direction="row" justify="center">
       <Card customStyle={{ cardStyle }}>
@@ -28,12 +76,12 @@ const ForumCard = ({currentuser, post}) =>{
           { current_user_id == user_id && 
           (
           <>
-            <Button  
+            <Button   
               variant={Button.VARIANT.TERTIARY_BACK}
               icon={<CDS.Edit />}>
                 Edit
             </Button>
-            <Button variant={Button.VARIANT.TERTIARY_BACK}
+            <Button onClick={() => handleOnDelete()} variant={Button.VARIANT.TERTIARY_BACK}
               icon={<CDS.Delete />}>
                 Delete
             </Button>
