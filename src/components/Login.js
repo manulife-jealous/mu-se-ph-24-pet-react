f9653f9f62d70ad2481aad8d6a7372b54dbdf917
Ian Eco <ian_paul_eco@manulife.ca>
2023-09-11 23:28:08 -0400

    Change password to verify

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,31 +8,31 @@ const Login = ({user}) => {
           name: "Ian Paul Eco",
           email: "ian_paul_eco@manulife.ca",
           phone: "123-4567",
-          password: "Awkward"
+          verify: "Awkward"
         },
         {
           id: 2,
           name: "Jealous Dizon",
           email: "jealous_dizon@manulife.ca",
           phone: "890-1234",
-          password: "Ambie Vert"
+          verify: "Ambie Vert"
         },
         {
           id: 3,
           name: "Christian Librea",
           email: "christian_librea@manulife.ca",
           phone: "567-8901",
-          password: "Gene Rosity"
+          verify: "Gene Rosity"
         },
       ]
 
     const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [verify, setVerify] = useState();
     const [currentUser, setUser] = useState(user);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const handleOnClick = () => {
-            setUser(users.filter(user => {return user.email === email && user.password === password}));
+            setUser(users.filter(user => {return user.email === email && user.verify === verify}));
             setIsLoggedIn(currentUser.length === 0 ? isLoggedIn : !isLoggedIn)
         }
     
@@ -41,7 +41,7 @@ const Login = ({user}) => {
     }
 
     const handleOnPassword = (event) => {
-        setPassword(event.target.value);
+        setVerify(event.target.value);
     }
 
     return (
@@ -51,7 +51,7 @@ const Login = ({user}) => {
         </div>
         <div class="_6lux">
             <div class="_6luy _55r1 _1kbt" id="passContainer">
-                <input type="password" onChange={handleOnPassword} value={password} class="inputtext _55r1 _6luy _9npi" name="pass" id="pass" data-testid="royal_pass" placeholder="Password" aria-label="Password" />
+                <input type="password" onChange={handleOnPassword} value={verify} class="inputtext _55r1 _6luy _9npi" name="pass" id="pass" data-testid="royal_pass" placeholder="Password" aria-label="Password" />
                 <div class="_9ls7 hidden_elem" id="u_0_3_Lx">
                     <a href="#" role="button">
                         <div class="_9lsa">
